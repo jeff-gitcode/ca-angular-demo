18fb18409693b31762b04d6a28c0aaf5f4f3e99d
jeff-gitcode <jeff.gitcode@gmail.com>
2023-12-07 23:41:53 +1100

    updated storybook

--- apps/ca-angular-demo/src/app/presentation/nav/nav.component.stories.ts
+++ apps/ca-angular-demo/src/app/presentation/nav/nav.component.stories.ts
@@ -1,12 +1,29 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { NavComponent } from './nav.component';
 
 import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
+import { expect, jest } from '@storybook/jest';
+import { IAuthUseCase } from '../../application/abstract/iauth.usecase';
 
 const meta: Meta<NavComponent> = {
   component: NavComponent,
   title: 'NavComponent',
+  decorators: [
+    moduleMetadata({
+      imports: [],
+      providers: [{
+        provide: IAuthUseCase, useValue: {
+          isLoggedIn: jest.fn(),
+          isLoggedOut: jest.fn(),
+          isLoggedInStatus: jest.fn(),
+          loginSignal: jest.fn(),
+          login: jest.fn(),
+          logout: jest.fn(),
+          getExpiration: jest.fn(),
+        }
+      }],
+    }),
+  ],
 };
 export default meta;
 type Story = StoryObj<NavComponent>;
